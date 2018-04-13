979c9aab4ae9dca0a4f3d57146562d90dccaa259
Iván Martínez <ivanmartinez33@outlook.com>
2018-04-12 20:13:44 -0500

    Proyecto creado

---
+++ Hero Guest/Hero Guest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hero Guest
+//
+//  Created by Iván Martínez on 4/12/18.
+//  Copyright © 2018 Iván Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
