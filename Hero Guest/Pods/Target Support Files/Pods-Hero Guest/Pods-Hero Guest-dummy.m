14bf9f80e8c435962a3d2d183320a55ab2d0ea15
Iván Martínez <ivanmartinez33@outlook.com>
2018-04-13 21:16:12 -0500

    Firebase instalado mediante pods

---
+++ Hero Guest/Pods/Target Support Files/Pods-Hero Guest/Pods-Hero Guest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hero_Guest : NSObject
+@end
+@implementation PodsDummy_Pods_Hero_Guest
+@end
---
+++ Hero Guest/Pods/Target Support Files/Pods-Hero GuestUITests/Pods-Hero GuestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hero_GuestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Hero_GuestUITests
+@end
